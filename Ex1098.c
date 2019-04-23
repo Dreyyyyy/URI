a3f9dee0298b468fd2a7a7917018fe648310564c
Andrey Naligatski Dias <43612323+Dreyyyyy@users.noreply.github.com>
2019-04-23 18:38:00 -0300

    URI

--- Ex1098.c
+++ Ex1098.c
@@ -1,15 +1,23 @@
 #include <stdio.h>
 
-int main(int argc, char const *argv[]) {
-  float i = 0, j = 1;
-  while (i <= 2) {
-    while (j <= 3.4) {
-      printf("I=%.1f J=%.1f\n", i, j);
-      j++;
+int main(void) {
+    float k = 0, I = 0, J = 1;
+    while (I <= 2.0) {
+        if ( I == 0 || I == 1 || I == 2) {
+            printf("I=%d J=%d\n", (int)I, (int)J);
+            printf("I=%d J=%d\n", (int)I, (int)J+1);
+            printf("I=%d J=%d\n", (int)I, (int)J+2);
+        }
+        else {
+            printf("I=%.1f J=%.1f\n", I, J);
+            printf("I=%.1f J=%.1f\n", I, J+1);
+            printf("I=%.1f J=%.1f\n", I, J+2);
+        }
+        I += 0.2;
+        J += 0.2;
     }
-    j = 1;
-    i += 0.2;
-    j += 0.2;
-  }
-  return 0;
+    printf("I=%d J=%d\n", (int)I, (int)J);
+    printf("I=%d J=%d\n", (int)I, (int)J+1);
+    printf("I=%d J=%d\n", (int)I, (int)J+2);    
+    return 0;
 }
