e2932a23b97a57783085e8af52df7c1f8d371d35
Dreyyyyy <andreydias@alunos.utfpr.edu.br>
2022-03-23 01:13:40 -0300

    First Java codes added

---
+++ java-beecrowd/src/ex02.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class ex02 {
+
+	public static void main(String[] args) {
+		int A = 0, B = 0, X = 0;
+		Scanner input = new Scanner(System.in);
+		
+		A = input.nextInt();
+		B = input.nextInt();
+		
+		X = A + B;
+		
+		System.out.println("X = " + X);
+	}
+
+}
---
+++ java-beecrowd/src/ex03.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class ex03 {
+
+	public static void main(String[] args) {
+		double n = 3.14159, raio = 0.0, area = 0.0;
+		Scanner input = new Scanner(System.in);
+		
+		raio = input.nextDouble();
+		
+		area = n * (raio * raio);
+		
+		System.out.printf("A=%.4f", area);
+	}
+
+}
